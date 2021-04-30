b60e9ddb12a6bf1e0d78f84215d6d7cff98c31f1
Slawek <twitczu4321@gmail.com>
2021-04-30 12:07:54 +0200

    Routing w ReactJS. Zadanie L8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import Posts from './components/posts';
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import NotFound from './components/notFound';
 
 function App() {
 	return (
-		<div className="container">
-			<Posts/>
-		</div>
+		<div className="container-fluid">
+               <NavBar />
+               <div className="container">
+                   <div className="content">
+                       <Switch>
+                       		<Route path="/posts"
+                       						render={props => <Posts sortBy="newest" {...props} />}
+                       		/>
+                       		<Route path="/" exact component={Home}/>
+                       		<NotFound/>
+                       	</Switch>
+                   </div>
+               </div>
+             </div>
+
 	);
 }
 
