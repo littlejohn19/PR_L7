ee4172ffaf2afb429c049995d9d052c9acbb5971
Bartłomiej Mirek <bwmirek@gmail.com>
2021-04-30 10:11:42 +0200

    react router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App/>
-	</React.StrictMode>,
+	<BrowserRouter>
+		<React.StrictMode>
+			<App/>
+		</React.StrictMode>
+	</BrowserRouter>,
 	document.getElementById('root'),
 );
 
