c5a8e5a5b854a3b968b7870ef2585c026550ff38
Marcin Psioda <marcin.psioda@onet.pl>
2021-04-30 12:07:40 +0200

    Zadanie L8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import Posts from './components/posts';
+import { Route, Switch } from "react-router-dom";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import NotFound from './components/notFound';
 
 function App() {
-    return (
-        <div className="container">
-            <Posts/>
-        </div>
-    );
+ return (
+     <div className="container-fluid">
+                 <NavBar/>
+                 <div className="container">
+                     <div className="content">
+                         <Switch>
+                             <Route path="/posts"
+                                          render={props => <Posts sortBy="newest" {...props} />}
+                             />
+                             <Route path="/" exact component={Home}/>
+                             <NotFound/>
+                         </Switch>
+                     </div>
+                 </div>
+             </div>
+ );
 }
 
 export default App;
+
