3988a1661ca0ba12c80da19203001326af329111
acronimuss <zozolandian0@gmail.com>
2021-04-30 12:06:46 +0200

    L7-PR+L8 - PR - Routing w ReactJS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import Posts from './components/posts';
-
-
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import NotFound from './components/notFound';
 
 function App() {
 	return (
-		<div className="container">
-			<Posts/>
+		<div className="container-fluid">
+			<NavBar/>
+			<div className="container">
+				<div className="content">
+					<Switch>
+						<Route path="/posts"
+									 render={props => <Posts sortBy="newest" {...props} />}
+						/>
+						<Route path="/" exact component={Home}/>
+						<NotFound/>
+					</Switch>
+				</div>
+			</div>
 		</div>
 	);
 }
