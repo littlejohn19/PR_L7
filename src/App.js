943dd6274258900bc3b5e305dad36e9e3aa887fe
Mateusz Taraszka <m.taraszka98@gmail.com>
2021-05-07 10:16:20 +0200

    L9

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/home";
 import NavBar from "./components/navbar";
 import { Route, Switch } from "react-router-dom";
 import Posts from './components/posts';
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div className="container">
             <div className="content">
                 <Switch>
+                <Route path="/login" component={LoginForm} />
                     <Route path="/posts"
                     render={props => <Posts sortBy="newest" {...props} />}
                     />
