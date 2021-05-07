1f6f895fae20ad6bf0d1d0f6d53dacd43ce5ad43
acronimuss <zozolandian0@gmail.com>
2021-05-07 11:07:15 +0200

    PR_L9 na PR_L7

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Posts from './components/posts';
 import Home from "./components/home";
 import NavBar from "./components/navbar";
 import NotFound from './components/notFound';
+import LoginForm from "./components/loginForm";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<div className="container">
 				<div className="content">
 					<Switch>
+                        <Route path="/login" component={LoginForm} />
 						<Route path="/posts"
 									 render={props => <Posts sortBy="newest" {...props} />}
 						/>
